d969c3feb143c98beda2ba788d13e7804fee0faa
Igor <igorltx@gmail.com>
2020-08-18 20:00:24 -0300

    Adicionada classe cadastro e programa

---
+++ teste/Cadastro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teste
+{
+    class Cadastro
+    {
+        public int ID { get; private set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Cadastro(int iD, string nome, string email)
+        {
+            ID = iD;
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return "ID: "
+                + ID
+                + ", "
+                + Nome
+                + ", "
+                + Email;
+        }
+    }
+}
--- teste/Program.cs
+++ teste/Program.cs
@@ -7,6 +7,35 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string[] vect = new string[10];
+            Console.WriteLine("Olá");
+            Console.WriteLine("Como vai?");
+            Console.WriteLine("Estou aprendendo C#");
+            Console.WriteLine();
+            Console.Write("Digite 7 coisas aí: ");
+            for (int i = 1; i < 8; i++)
+            {
+                Console.Write($"#{i} :");
+                vect[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Bora fazer um cadastro?");
+            Console.WriteLine("Quantos cadastros serão realizados?");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Cadastro cad;
+            for (int i = 1; i <= n; i++)
+            {
+                int id = i;
+                Console.Write("Digite o nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o email: ");
+                string email = Console.ReadLine();
+                cad = new Cadastro(id, nome, email);
+                Console.WriteLine();
+                Console.WriteLine(cad);
+                Console.WriteLine();
+            }           
         }
     }
 }
